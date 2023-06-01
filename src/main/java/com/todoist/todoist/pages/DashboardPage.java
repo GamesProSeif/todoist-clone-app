8a3a5e6729cf130d238402126d90c0b3d091276b
Kareem Abo Elazm <3aseerelsabbar@gmail.com>
2023-06-01 22:34:42 +0300

    Updated UI

--- src/main/java/com/todoist/todoist/pages/DashboardPage.java
+++ src/main/java/com/todoist/todoist/pages/DashboardPage.java
@@ -2,9 +2,13 @@ package com.todoist.todoist.pages;
 
 import com.todoist.todoist.structures.Page;
 import com.todoist.todoist.structures.TodoistApp;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 
 public class DashboardPage extends Page {
     public DashboardPage(TodoistApp app) {
@@ -13,8 +17,29 @@ public class DashboardPage extends Page {
     @Override
     public Pane getContent() {
         BorderPane pane = new BorderPane();
-        Label label = new Label("Dashboard Page");
-        pane.setCenter(label);
+        Label label = new Label("    Dashboard Page    ");
+        label.getStyleClass().addAll("h3","b","alert-success","panel-primary","btn-lg");
+
+        VBox vbox1 = new VBox();
+        HBox hbox1 = new HBox();
+
+        Label l1 = new Label("Due Today: ");
+        l1.getStyleClass().addAll("h4","b","text-info");
+
+        Label l2 = new Label("Projects: ");
+        l2.getStyleClass().addAll("h4","b","text-info");
+
+        hbox1.getChildren().addAll(l1);
+        HBox hbox2 = new HBox();
+        hbox2.getChildren().addAll(l2);
+        label.setAlignment(Pos.TOP_CENTER);
+        vbox1.getChildren().addAll(label,hbox1,hbox2);
+        vbox1.setSpacing(75);
+        vbox1.setAlignment(Pos.CENTER);
+
+
+        pane.setCenter(vbox1);
+
 
         return pane;
     }
--- src/main/java/com/todoist/todoist/pages/ProjectPage.java
+++ src/main/java/com/todoist/todoist/pages/ProjectPage.java
@@ -3,10 +3,15 @@ package com.todoist.todoist.pages;
 import com.todoist.todoist.models.Project;
 import com.todoist.todoist.structures.Page;
 import com.todoist.todoist.structures.TodoistApp;
+import javafx.scene.Group;
 import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.text.Font;
 
 public class ProjectPage extends Page {
     Project project;
@@ -23,11 +28,22 @@ public class ProjectPage extends Page {
         vbox.getChildren().add(label);
         pane.setCenter(vbox);
 
+        HBox hbox1 = new HBox();
+        Group grp = new Group();
+
+        label.getStyleClass().addAll("panel-heading","alert-info","text-heading","h4","b","btn-lg");
+
         var tasks = app.taskController.list();
         tasks.forEach((id, task) -> {
             if (task.project.id.equals(project.id)) {
                 Label taskLabel = new Label(task.title);
-                vbox.getChildren().add(taskLabel);
+                //taskLabel.getStyleClass().addAll("panel-primary","h6","bg-primary","text-heading");
+                //Rectangle r = new Rectangle(100,100);
+                //r.setFill(Color.AQUA);
+                //grp.getChildren().addAll(taskLabel);
+
+                hbox1.getChildren().addAll(taskLabel);
+                vbox.getChildren().add(hbox1);
             }
         });
 
